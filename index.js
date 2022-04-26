4385e390f37b85a221701c73958d66731bd41724
toshiki16 <toshiki6226@icloud.com>
2022-04-26 23:17:11 +0900

    6回目のコミット

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const Tenmavoice1 = new Audio('./sound-effect/aiueo');
-
-// function
-const TenmaButton = () => {
-  alert("aiueoo");
-  console.log('aiueo');
+window.onload = function () {
+  var my_audio = new Audio("./sound-effect/天空の反逆者.mp3");
+  //ボタンにクリックイベントを設定
+  document.getElementById("tenmaButton").onclick = function () {
+    my_audio.currentTime = 0;  //再生開始位置を先頭に戻す
+    my_audio.play();  //サウンドを再生
+  }
 }
-
