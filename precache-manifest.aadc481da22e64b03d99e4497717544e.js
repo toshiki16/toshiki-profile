e59da24203c907740cb1499578f348e3bad950a5
toshiki16 <toshiki6226@icloud.com>
2022-05-01 01:43:07 +0900

    Updates

---
+++ precache-manifest.aadc481da22e64b03d99e4497717544e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcb8e22ea107efef7295",
+    "url": "/toshiki-profile/static/css/main.cb522a54.chunk.css"
+  },
+  {
+    "revision": "dcb8e22ea107efef7295",
+    "url": "/toshiki-profile/static/js/main.b01b9504.chunk.js"
+  },
+  {
+    "revision": "028e852ca5dc8eb94aa3",
+    "url": "/toshiki-profile/static/js/runtime~main.c15d78b8.js"
+  },
+  {
+    "revision": "20d4dbd5b0646bcae40f",
+    "url": "/toshiki-profile/static/js/2.da5df06d.chunk.js"
+  },
+  {
+    "revision": "9ccbb90a74279128d478198b352bc179",
+    "url": "/toshiki-profile/static/media/snake.9ccbb90a.png"
+  },
+  {
+    "revision": "206386b19a198528e464806a301f1d54",
+    "url": "/toshiki-profile/static/media/markdown.206386b1.png"
+  },
+  {
+    "revision": "ba7d461ec8058728dbb6becc1e5ecdb8",
+    "url": "/toshiki-profile/static/media/Python-cpu.ba7d461e.png"
+  },
+  {
+    "revision": "de0d3e4d821e1bfc6ecb8415dd98ebd4",
+    "url": "/toshiki-profile/static/media/patissier-perchoir.de0d3e4d.jpg"
+  },
+  {
+    "revision": "2004e6bf8b0ce42cb26de654802c9a36",
+    "url": "/toshiki-profile/index.html"
+  }
+];
